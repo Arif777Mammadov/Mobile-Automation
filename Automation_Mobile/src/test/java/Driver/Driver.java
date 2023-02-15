0938ba5b486d261e54d96e16d3d8e14357a5562d
Arif777Mammadov <119847058+Arif777Mammadov@users.noreply.github.com>
2023-02-15 14:57:17 +0400

    Update Driver.java

--- Automation_Mobile/src/test/java/Driver/Driver.java
+++ Automation_Mobile/src/test/java/Driver/Driver.java
@@ -9,9 +9,10 @@ import java.net.URL;
 public class Driver {
     public static AndroidDriver getDriver() throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("deviceName", "R58MA5SFA7F");
+        caps.setCapability("deviceName", "********");//Put your device name here, you can learn it via 'adb devices'
         caps.setCapability("platformName", "Android");
-        caps.setCapability("app", "C:/Users/USER/Downloads/com.beermoneygames.demolition3_611083_apps.evozi.com.apk");
+        caps.setCapability("app", "C:/Users/******/Downloads/com.beermoneygames.demolition3_611083_apps.evozi.com.apk");
+        //Enter your user-name, or enter any other directory, where this .apk file will be located
         caps.setCapability("appWaitActivity", "*");
         caps.setCapability("noReset", "true");
         caps.setCapability("fullReset", "false");
